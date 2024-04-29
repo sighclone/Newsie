35b3142a52329c762b2038fea2209120bbb1b44a
Abhinav Sadhu <sadhu.Abhinav@gmail.com>
2024-04-29 17:37:44 +0530

    show overall sentiment

--- generate_results.py
+++ generate_results.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
+import numpy as np
 
 # Read the Excel file
 df = pd.read_excel('data_with_images.xlsx')
@@ -18,5 +19,16 @@ scatter = ax.scatter(df.index, df['polarity'], c=df['subjectivity'], cmap=cmap)
 plt.colorbar(ScalarMappable(norm=Normalize(vmin=df['subjectivity'].min(), vmax=df['subjectivity'].max()), cmap=cmap), ax=ax, label='Subjectivity')
 ax.set_xlabel('Index')
 ax.set_ylabel('Polarity')
-ax.set_title('Analysis Results')
+ax.set_title('Analysis Results - Overall Sentiment')
 plt.show()
+
+polarity = 0
+df = np.array(df)
+for entry in df:
+    polarity+=entry[2]
+print("Mean Polarity: ", polarity/len(df))
+
+subjectivity = 0
+for entry in df:
+    subjectivity+=entry[3]
+print("Mean Subjectivity: ", subjectivity/len(df))
